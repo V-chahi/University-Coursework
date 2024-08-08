c348199b3fbac34188bd5002ae97cad89dc67a98
V-chahi <161653292+V-chahi@users.noreply.github.com>
2024-08-08 13:16:37 +0200

    pushing all

---
+++ Programming-in-CPP/A9/A9P1.cpp
@@ -0,0 +1,21 @@
+/*
+CH-230-A
+A9_P1.[cpp]
+m.chahi@jacobs-university.de
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char s[100];
+
+    cin>>s;
+
+   			 //Output to screen
+
+    cout<<s<<'\n';
+    return 0;
+}
